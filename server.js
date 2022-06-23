cb36904a4d3a414fd7ab7b73166dedc0ba23ed62
Brayan <basecoco1@gmail.com>
2022-06-22 18:31:38 -0600

    fixing port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // }).catch(() => {
 //     console.log("Failed to connect to DB")
 // });
-const port = 3000;
+const port = process.env.PORT || 3000;
 const protectedRoute = express.Router();
 
 app.use(express.json());
